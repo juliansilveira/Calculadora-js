daf8f148926ed8f760ed643a1d4c51f8b2bf90be
juliansilveira <julian.silveira.annoni@gmail.com>
2021-11-28 22:45:33 -0300

    primera vez subiendo a github

---
+++ Calculadoraclass.js
@@ -0,0 +1,14 @@
+class Calculadora{
+    sumar(numero1, numero2) {
+        return numero1 + numero2;
+    }
+    restar(numero1, numero2) {
+        return numero1 - numero2;
+    }
+    dividir(numero1, numero2) {
+        return numero1 / numero2;
+    }
+    multiplicar(numero1, numero2) {
+        return numero1 * numero2;
+    }
+}
